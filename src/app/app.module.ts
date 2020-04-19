7a0ff7f5be7f1b44d43f298a7a22c2554efb248d
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-04-19 13:06:59 +0200

    Updated routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent],
