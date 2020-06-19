639357814d5aa5d7a814614e2fb93f67444fd014
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-06-19 23:35:29 +0200

    Created toggle for changing user role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rent-a-car';
+  role: string = localStorage.getItem('User-role');
+
+  ngOnInit(): void {
+    localStorage.setItem('User-role', 'ROLE_AGENT');
+  }
+
+  change() {
+    if (localStorage.getItem('User-role') == 'ROLE_USER')
+      localStorage.setItem('User-role', 'ROLE_AGENT');
+    else
+      localStorage.setItem('User-role', 'ROLE_USER');
+    this.role = localStorage.getItem('User-role');
+  }
 }
