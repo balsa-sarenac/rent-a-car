de5c9df4cd7ec7a3bc44da64aa4b306702f2ec00
Stefan Kovač <stefankovac42@gmail.com>
2020-06-19 18:38:48 +0200

    added view for a car

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CarService } from './shared/car.service';
+import { CommentService } from '../comment/shared/comment.service';
+import { IComment } from '../comment/shared/comment';
+import { AdInfo } from './shared/adInfo';
 
 @Component({
   selector: 'app-car',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
-  constructor() { }
+  ad: AdInfo;
+  commentText: string;
+  comment: IComment;
+  comments: IComment[];
+
+  constructor(private carService: CarService,
+              private commentService: CommentService) { }
 
   ngOnInit(): void {
+    this.carService.getOneAd(5).subscribe(
+      data => {this.ad = data;
+        this.commentService.getCommentsForCar(this.ad.car.id).subscribe(
+          data => {this.comments = data
+          }
+        );
+      }
+    );
+    
+  } 
+
+  sendComment(): void{
+    this.comment = {
+        adId: this.ad.id,
+        approved: false,
+        text: this.commentText,
+        userUsername: 'agent',
+        id: null,
+        carId: null
+    };
+    this.commentService.createComment(this.comment).subscribe();
+    this.commentText = "";
   }
 
 }
