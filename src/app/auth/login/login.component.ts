27be3751c386fd08537a21fb5549380273fe2740
Stefan Kovač <stefankovac42@gmail.com>
2020-06-24 20:09:01 +0200

    implemented login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +11,35 @@ import { FormBuilder } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = this.formBuilder.group({
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService, 
+    private router: Router) {
+      this.loginForm = this.formBuilder.group({
       username: '',
       password: '',
-    });
-  }
+      });
+}
 
   ngOnInit(): void {}
 
   onSubmit(userData: Object): void {
     // sent to back-end
+    this.authService.login(userData).subscribe(
+      data=>{
+        localStorage.setItem("User-token", data.accessToken);
+        localStorage.setItem("Expires-in", data.expiresIn);
+        localStorage.setItem("Refresh-token", data.refreshToken);
+        localStorage.setItem("Username", data.username);
+        localStorage.setItem("User-role", data.role);
+
+        if(data.role == 'ROLE_ADMIN'){
+          this.router.navigate(['adminHomepage']);
+        }else if(data.role != ''){
+          this.router.navigate(['usersHomepage']);
+        }
+        
+      }
+    );
     this.loginForm.reset();
   }
 }
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getToken() {
     let token = localStorage.getItem("User-token");
     return token == null ? '' : 'Bearer ' + token;
+  }
+
+  login(login: any) {
+    return this.http.post<any>('http://localhost:8080/login', login);
+    //return this.http.post<any>(environment.api + '/security/login', login);
   }
 }
