c271c213c6f75bb7258ee5d08c4e17566d60c7fd
Stefan Kovač <stefankovac42@gmail.com>
2020-06-24 20:10:21 +0200

    created homepage for unregistred users

---
+++ src/app/homepage/homepage-unregistred/homepage-unregistred.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-unregistred',
+  templateUrl: './homepage-unregistred.component.html',
+  styleUrls: ['./homepage-unregistred.component.css']
+})
+export class HomepageUnregistredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
