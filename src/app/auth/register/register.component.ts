988c4f42c584bcca42dbf0ba18d679bbd3331856
Stefan Kovač <stefankovac42@gmail.com>
2020-06-28 18:29:22 +0200

    implemented registration
    added necessary fields in forms, and created calls for methods on back

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Form, FormGroup } from '@angular/forms';
+import { User } from 'src/app/car/shared/user';
+import { AuthService } from '../shared/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -8,20 +11,63 @@ import { FormBuilder, Form, FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   registerForm;
+  role: string = '';
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private _toastr: ToastrService) {
     this.registerForm = this.formBuilder.group({
       firstName: '',
       lastName: '',
       address: '',
       email: '',
       password: '',
+      companyName: '',
+      businessID: '',
+      username:'',
     });
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.role = localStorage.getItem('User-role');
+  }
 
   onSubmit(userData) {
+    let user: User ={
+      id: null,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      username: userData.username,
+      companyName: '',
+      isAdmin: false,
+      enabled: false,
+      address: userData.address,
+      businessID: '',
+      email: userData.email,
+      password: userData.password,
+      roles: []
+    };
+
+    if(this.role == 'ROLE_ADMIN'){
+      user.businessID = userData.businessID;
+      user.companyName = userData.companyName;
+      user.roles.push("ROLE_AGENT");
+    }else{
+      user.roles.push("ROLE_USER");
+    }
+
+    this.authService.register(user).subscribe(
+      data=> {
+        if(this.role == 'ROLE_ADMIN')
+            this._toastr.success("You have succesfully added agent", "Registration");
+        else
+            this._toastr.success("You have succesfully sent reqistration request", "Registration");
+
+      }, 
+      error =>{
+        this._toastr.error("Error during registration", "Registration");
+      }
+    );
     this.registerForm.reset();
   }
 }
