48efd0646fbadb389e3cd47ed85d50ee832e9661
Stefan Kovač <stefankovac42@gmail.com>
2020-07-01 10:33:44 +0200

    added notifications on accepting comments

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IComment } from './shared/comment';
 import { CommentService } from './shared/comment.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-comment',
@@ -11,40 +12,40 @@ export class CommentComponent implements OnInit {
 
   comments: IComment[];
 
-  constructor(private commentService: CommentService) { }
+  constructor(private commentService: CommentService, private _toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.commentService.getComments().subscribe(
         (data: IComment[]) =>   this.comments = data,
-        err => console.error('Error in getting comments')
+        err => this._toastr.error("Error getting comments", "Comments")
         );
   }
   refresh(){
     this.commentService.getComments().subscribe(
        (data: IComment[]) => this.comments = data,
-       err => console.error('Error in getting comments')
+       err => this._toastr.error("Error getting comments", "Comments")
        );
  }
 
   onClickRefuse(comment: any){
     this.commentService.acceptOrRefuse(comment.id, false).subscribe(
       data => {
-          console.log('Comment refused');
+          this._toastr.success("Comment " + comment.text +" refused", "Comment")
           this.refresh();
       },
       err => {
-          console.error('Error accepting comment');
+        this._toastr.error("Error refusing comment", "Comment")
       });
   }
 
   onClickAccepte(comment: any){
     this.commentService.acceptOrRefuse(comment.id, true).subscribe(
       data => {
-          console.log('Comment accepted');
+          this._toastr.success("Comment " + comment.text +" accepted", "Comment")
           this.refresh();
       },
       err => {
-          console.error('Error accepting comment');
+        this._toastr.error("Error accepting comment", "Comment")
       });
   }
 
