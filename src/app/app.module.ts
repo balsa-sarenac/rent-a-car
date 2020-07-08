870f6cb5a80d6b5a38c15344e2cf5c229ebf1ca2
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-07-08 22:58:29 +0200

    Created new component for sending requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { CommentAndGradeComponent } from './comment-and-grade/comment-and-grade.
 import { PriceListComponent } from './price-list/price-list.component';
 import { ProfileComponent } from './auth/profile/profile.component';
 import { BillComponent } from './bill/bill.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
@@ -57,6 +58,7 @@ import { BillComponent } from './bill/bill.component';
     PriceListComponent,
     ProfileComponent,
     BillComponent,
+    ShoppingCartComponent,
   ],
   imports: [NgbModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule,
             ToastrModule.forRoot({
