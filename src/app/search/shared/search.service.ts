5a85a83660b285bc13c2bfea830643ef5842e6cd
Stefan Kovač <stefankovac42@gmail.com>
2020-07-10 16:49:36 +0200

    changed routes to microservices

--- src/app/search/shared/search.service.ts
+++ src/app/search/shared/search.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Search } from './search';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   searchAds(search: Search, page: number, sort: string) {
-    //return this.http.post<any>(environment.api + "search/ad/search/"+ page+"/"+sort, search);
-    return this.http.post<any>("http://localhost:8080/ad/search/"+ page+"/"+sort,search);
+    return this.http.post<any>(environment.api + "/search/ad/search/"+ page+"/"+sort, search);
+    //return this.http.post<any>("http://localhost:8080/ad/search/"+ page+"/"+sort, search);
   }
 }
