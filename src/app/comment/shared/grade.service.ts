d44910cc51fdda02a74569a0f7af0f571c51378f
Stefan Kovač <stefankovac42@gmail.com>
2020-07-12 09:05:19 +0200

    Update grade.service.ts

--- src/app/comment/shared/grade.service.ts
+++ src/app/comment/shared/grade.service.ts
@@ -11,18 +11,18 @@ export class GradeService {
 
 
 createGrade(grade: any){
-  //return this.http.post('http://localhost:8080/grade', grade);
-   return this.http.post(environment.api + '/car/grade', grade);
+  return this.http.post('http://localhost:8080/grade', grade);
+  //  return this.http.post(environment.api + '/car/grade', grade);
 }
 
 checkGrade(id: number, username: string) {
-  //return this.http.get<any>('http://localhost:8080/grade/check/' + username + "/" + id);
-  return this.http.get<any>(environment.api + '/car/grade/check/' + username + "/" + id);
+  return this.http.get<any>('http://localhost:8080/grade/check/' + username + "/" + id);
+  // return this.http.get<any>(environment.api + '/car/grade/check/' + username + "/" + id);
 }
 
 getGradesForCar(carId: number) {
-  //return this.http.get<any>('http://localhost:8080/grade/car/' + carId);
-  return this.http.get<any>(environment.api + '/car/grade/car/' + carId);
+  return this.http.get<any>('http://localhost:8080/grade/car/' + carId);
+  // return this.http.get<any>(environment.api + '/car/grade/car/' + carId);
 }
 
 }
